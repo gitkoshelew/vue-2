e39ac31dda06f303b4eee491f218455f6b3365ea
Dzmitry_Koshaleu <d.koshelev@arateg.by>
2019-05-30 17:17:06 +0300

    progect arch

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <h1>try text</h1>
-    <app-input></app-input>
+    <h1>{{ message + "!!" }}</h1>
+    <app-input :msg="inputMessage" @inputMsgChange="inputMessage = $event"></app-input>
   </div>
 </template>
 
 <script>
 import Input from "./Field.vue";
 export default {
+  data() {
+    return {
+      inputMessage: "mess from inpu"
+    };
+  },
   components: {
     "app-input": Input
   }
--- src/components/Field.vue
+++ src/components/Field.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <h3>{{ "props " + msg }}</h3>
+    <input type="text" :value="msg" @input="changeMess">
     <input type="text" v-model="mess">
     <p>{{ mess }}</p>
   </div>
@@ -7,10 +9,17 @@
 
 <script>
 export default {
+  props: ["msg"],
   data() {
     return {
       mess: ""
     };
+  },
+  methods: {
+    changeMess(event) {
+      this.mess = event.target.value;
+      this.$emit("inputMsgChange", this.mess);
+    }
   }
 };
 </script>
